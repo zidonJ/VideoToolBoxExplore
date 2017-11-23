a685053ce9335189b89707ee3aeeaebe7a03470b
zidonj <zidonj@163.com>
2017-11-23 10:29:57 +0800

    录像

--- VideoToolBoxExp/ViewController.m
+++ VideoToolBoxExp/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by 姜泽东 on 2017/11/22.
 //  Copyright © 2017年 MaiTian. All rights reserved.
-//
+//  只支持iOS10_0以后
 
 #import "ViewController.h"
 #import <VideoToolbox/VideoToolbox.h>
@@ -14,7 +14,11 @@ static NSString * const videoUrl =
 @"http://play.g3proxy.lecloud.com/vod/v2/MTk1LzI5LzExMC9sZXR2LXV0cy8xNC92ZXJfMDBfMjItMTAzNjU0NzA5NS1hdmMtMzg0MjE2LWFhYy0zMjAwMC0yOTYwMjQ4LTE1NzU0OTU0My05YzQzNzEyOGZjMTk4ZTYwYjBlMWQ3MjY2Njg3NjU2ZS0xNDYwMDg4NTk0MzYyLm1wNA==?b=425&mmsid=51338425&tm=1464600702&key=479f9a6f02032998f3d30b0e751610b5&platid=3&splatid=355&playid=0&tss=ios&vtype=13&cvid=3528594867&payff=0&pip=28ca774f67782e24a64ddbfdb9e43292";
 
 @interface ViewController ()<AVCapturePhotoCaptureDelegate,AVCaptureVideoDataOutputSampleBufferDelegate>
-
+{
+    
+    CALayer *_customPreviewLayer;
+    UIImageView *_imgView;
+}
 @property (nonatomic,assign) BOOL isFlash;
 
 @property (nonatomic,strong) AVCaptureSession *session;
@@ -71,7 +75,6 @@ static NSString * const videoUrl =
     
     [self.photoOutPut capturePhotoWithSettings:setting delegate:self];
     
-    
     self.videoOutPut.videoSettings =
     [NSDictionary dictionaryWithObject:[NSNumber numberWithUnsignedInt:kCVPixelFormatType_420YpCbCr8BiPlanarFullRange]
                                                                 forKey:(NSString *)kCVPixelBufferPixelFormatTypeKey];
@@ -80,9 +83,17 @@ static NSString * const videoUrl =
     
     [self.videoOutPut setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
     
-
     
     [self.session startRunning];
+    
+    
+    _customPreviewLayer = [CALayer new];
+    _customPreviewLayer.frame = CGRectMake(10, 310, 300, 300);
+//    [self.view.layer addSublayer:_customPreviewLayer];
+    
+    _imgView = [UIImageView new];
+    _imgView.frame = CGRectMake(10, 310, 300, 300);
+    [self.view addSubview:_imgView];
 }
 
 #pragma mark -- button Action
@@ -162,7 +173,27 @@ didFinishProcessingPhotoSampleBuffer:(nullable CMSampleBufferRef)photoSampleBuff
 - (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
 {
     NSLog(@"录像啦");
+    CVImageBufferRef imageBuffer =  CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(imageBuffer,0);
+    size_t width = CVPixelBufferGetWidthOfPlane(imageBuffer, 0);
+    size_t height = CVPixelBufferGetHeightOfPlane(imageBuffer, 0);
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0);
+    //视频缓冲区中是YUV格式的,要从缓冲区中提取luma部分
+    uint8_t *lumaBuffer = CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0);
     
+    CGColorSpaceRef grayColorSpace = CGColorSpaceCreateDeviceGray();
+    CGContextRef context = CGBitmapContextCreate(lumaBuffer, width, height, 8, bytesPerRow, grayColorSpace, kCGImageAlphaNone);
+    
+    CGImageRef dstImage = CGBitmapContextCreateImage(context);
+
+    UIImage *image = [UIImage imageWithCGImage:dstImage scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp];
+    _imgView.image = image;
+//    NSLog(@"%@",image);
+//    _customPreviewLayer.contents = (__bridge id _Nullable)(image.CGImage);
+
+    CGImageRelease(dstImage);
+    CGContextRelease(context);
+    CGColorSpaceRelease(grayColorSpace);
 }
 
 - (void)captureOutput:(AVCaptureOutput *)output didDropSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
@@ -222,7 +253,7 @@ didFinishProcessingPhotoSampleBuffer:(nullable CMSampleBufferRef)photoSampleBuff
     
     if (!_videoCaptureLayer) {
         _videoCaptureLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
-        _videoCaptureLayer.frame = CGRectMake(0, 0, self.view.frame.size.width, 500);
+        _videoCaptureLayer.frame = CGRectMake(0, 0, self.view.frame.size.width, 300);
         _videoCaptureLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
     }
     return _videoCaptureLayer;
